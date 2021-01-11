e430553f96eaab496fc54fb5a9cb7a06662b2821
DevExpressExampleBot <fakeEmail>
2021-01-11 17:18:55 +0300

    Source auto update [skip ci]

--- VB/CustomDrawExample/Form1.vb
+++ VB/CustomDrawExample/Form1.vb
@@ -1,21 +1,22 @@
-﻿Imports Microsoft.VisualBasic
-#Region "usings"
+﻿#Region "#usings"
+Imports DevExpress.Spreadsheet
+Imports DevExpress.XtraSpreadsheet
 Imports System
 Imports System.Drawing
 Imports System.Windows.Forms
-Imports DevExpress.Spreadsheet
-#End Region ' usings
+#End Region ' #usings
 
 Namespace CustomDrawExample
 	Partial Public Class Form1
 		Inherits Form
+
 		Public Sub New()
 			InitializeComponent()
 			spreadsheetControl1.LoadDocument("CustomDrawSample.xlsx")
 
 			spreadsheetControl1.Document.DocumentSettings.R1C1ReferenceStyle = True
-			spreadsheetControl1.Options.Behavior.Column.Delete = DevExpress.XtraSpreadsheet.DocumentCapability.Disabled
-			spreadsheetControl1.Options.Behavior.Column.Insert = DevExpress.XtraSpreadsheet.DocumentCapability.Disabled
+			spreadsheetControl1.Options.Behavior.Column.Delete = DocumentCapability.Disabled
+			spreadsheetControl1.Options.Behavior.Column.Insert = DocumentCapability.Disabled
 
 			AddHandler spreadsheetControl1.CustomDrawColumnHeader, AddressOf spreadsheetControl1_CustomDrawColumnHeader
 			AddHandler spreadsheetControl1.CustomDrawColumnHeaderBackground, AddressOf spreadsheetControl1_CustomDrawColumnHeaderBackground
@@ -25,44 +26,48 @@ Namespace CustomDrawExample
 			AddHandler spreadsheetControl1.CustomDrawCellBackground, AddressOf spreadsheetControl1_CustomDrawCellBackground
 		End Sub
 		#Region "#CustomDrawColumnHeader"
-		Private Sub spreadsheetControl1_CustomDrawColumnHeader(ByVal sender As Object, ByVal e As DevExpress.XtraSpreadsheet.CustomDrawColumnHeaderEventArgs)
+		Private Sub spreadsheetControl1_CustomDrawColumnHeader(ByVal sender As Object, ByVal e As CustomDrawColumnHeaderEventArgs)
 			e.Handled = True
-			Dim foreColor As Color = Color.Blue
+'INSTANT VB NOTE: The variable foreColor was renamed since Visual Basic does not handle local variables named the same as class members well:
+			Dim foreColor_Conflict As Color = Color.Blue
 			Dim textBounds As Rectangle = e.Bounds
-			Dim headingFont As New Font(e.Font, FontStyle.Italic)
+			e.Appearance.FontStyleDelta = FontStyle.Italic
 			Dim settingsSheet As Worksheet = spreadsheetControl1.Document.Worksheets("SheetSettings")
-			Dim text As String = settingsSheet.Cells(0, e.ColumnIndex).DisplayText
-			If text <> String.Empty Then
+'INSTANT VB NOTE: The variable text was renamed since Visual Basic does not handle local variables named the same as class members well:
+			Dim text_Conflict As String = settingsSheet.Cells(0, e.ColumnIndex).DisplayText
+			If text_Conflict <> String.Empty Then
 				Dim formatHeaderText As New StringFormat()
 				formatHeaderText.LineAlignment = StringAlignment.Center
 				formatHeaderText.Alignment = StringAlignment.Center
 				formatHeaderText.Trimming = StringTrimming.EllipsisCharacter
-				e.Graphics.DrawString(text, headingFont, e.Cache.GetSolidBrush(foreColor), textBounds, formatHeaderText)
+				e.Graphics.DrawString(text_Conflict, e.Font, e.Cache.GetSolidBrush(foreColor_Conflict), textBounds, formatHeaderText)
 			End If
 		End Sub
 		#End Region ' #CustomDrawColumnHeader
 
 		#Region "#CustomDrawColumnHeaderBackground"
-		Private Sub spreadsheetControl1_CustomDrawColumnHeaderBackground(ByVal sender As Object, ByVal e As DevExpress.XtraSpreadsheet.CustomDrawColumnHeaderBackgroundEventArgs)
+		Private Sub spreadsheetControl1_CustomDrawColumnHeaderBackground(ByVal sender As Object, ByVal e As CustomDrawColumnHeaderBackgroundEventArgs)
 			e.Handled = True
 			Dim is_selected As Boolean = e.IsHovered OrElse (e.ColumnIndex = spreadsheetControl1.ActiveCell.ColumnIndex)
-			Dim backColor As Color = If(is_selected, Color.Yellow, Color.White)
-			e.Cache.FillRectangle(e.Cache.GetSolidBrush(backColor), e.Bounds)
+'INSTANT VB NOTE: The variable backColor was renamed since Visual Basic does not handle local variables named the same as class members well:
+			Dim backColor_Conflict As Color = If(is_selected, Color.Yellow, Color.White)
+			e.Cache.FillRectangle(e.Cache.GetSolidBrush(backColor_Conflict), e.Bounds)
 		End Sub
 		#End Region ' #CustomDrawColumnHeaderBackground
 
 		#Region "#CustomDrawRowHeader"
-		Private Sub spreadsheetControl1_CustomDrawRowHeader(ByVal sender As Object, ByVal e As DevExpress.XtraSpreadsheet.CustomDrawRowHeaderEventArgs)
+		Private Sub spreadsheetControl1_CustomDrawRowHeader(ByVal sender As Object, ByVal e As CustomDrawRowHeaderEventArgs)
 			e.Handled = True
 
 			If (e.RowIndex + 1) Mod 5 = 0 Then
-				Dim font As System.Drawing.Font = New Font(e.Font.FontFamily, e.Font.Size, FontStyle.Bold)
+				e.Appearance.FontStyleDelta = FontStyle.Bold
 				Dim textBounds As Rectangle = e.Bounds
-				Dim text As String = (e.RowIndex + 1).ToString()
+'INSTANT VB NOTE: The variable text was renamed since Visual Basic does not handle local variables named the same as class members well:
+				Dim text_Conflict As String = (e.RowIndex + 1).ToString()
 				Dim formatHeaderText As New StringFormat()
 				formatHeaderText.LineAlignment = StringAlignment.Center
 				formatHeaderText.Alignment = StringAlignment.Center
-				e.Graphics.DrawString(text, font, e.Cache.GetSolidBrush(Color.Red), textBounds, formatHeaderText)
+				e.Graphics.DrawString(text_Conflict, e.Font, e.Cache.GetSolidBrush(Color.Red), textBounds, formatHeaderText)
 			Else
 				'e.DrawDefault();
 			End If
@@ -70,35 +75,39 @@ Namespace CustomDrawExample
 		#End Region ' #CustomDrawRowHeader
 
 		#Region "#CustomDrawRowHeaderBackground"
-		Private Sub spreadsheetControl1_CustomDrawRowHeaderBackground(ByVal sender As Object, ByVal e As DevExpress.XtraSpreadsheet.CustomDrawRowHeaderBackgroundEventArgs)
+		Private Sub spreadsheetControl1_CustomDrawRowHeaderBackground(ByVal sender As Object, ByVal e As CustomDrawRowHeaderBackgroundEventArgs)
 			e.Handled = True
 			Dim is_selected As Boolean = e.IsHovered OrElse (e.RowIndex = spreadsheetControl1.ActiveCell.RowIndex)
-			Dim backColor As Color = If(is_selected, Color.Yellow, Color.White)
-			e.Cache.FillRectangle(e.Cache.GetSolidBrush(backColor), e.Bounds)
+'INSTANT VB NOTE: The variable backColor was renamed since Visual Basic does not handle local variables named the same as class members well:
+			Dim backColor_Conflict As Color = If(is_selected, Color.Yellow, Color.White)
+			e.Cache.FillRectangle(e.Cache.GetSolidBrush(backColor_Conflict), e.Bounds)
 		End Sub
 		#End Region ' #CustomDrawRowHeaderBackground
 
 		#Region "#CustomDrawCell"
-		Private Sub spreadsheetControl1_CustomDrawCell(ByVal sender As Object, ByVal e As DevExpress.XtraSpreadsheet.CustomDrawCellEventArgs)
+		Private Sub spreadsheetControl1_CustomDrawCell(ByVal sender As Object, ByVal e As CustomDrawCellEventArgs)
 			If e.Cell.RowIndex = 0 OrElse e.Cell.RowIndex >= 3 Then
-				Dim headingFont As New Font("Times New Roman", e.Font.Size)
-				Dim cellRef As String = e.Cell.GetReferenceR1C1(ReferenceElement.RowAbsolute Or ReferenceElement.ColumnAbsolute, Nothing)
-				Dim formula As String = String.Format("=RANK.AVG({0},R{1}C{2}:R{3}C{4})", cellRef, e.Cell.RowIndex + 1, 2, e.Cell.RowIndex + 1, 10)
-				Dim rank As Integer = CInt(Fix(spreadsheetControl1.Document.Evaluate(formula).NumericValue))
-				Dim rankText As String = DevExpress.Docs.Text.NumberInWords.Cardinal.ConvertToText(rank, DevExpress.Docs.Text.NumberCulture.Roman)
-				If rank > 0 AndAlso rank < 4 Then
-					e.Graphics.DrawString(rankText, headingFont, e.Cache.GetSolidBrush(Color.Red), e.Bounds.Left, e.Bounds.Top)
-				End If
+				Using headingFont As New Font("Times New Roman", e.Font.Size)
+					Dim cellRef As String = e.Cell.GetReferenceR1C1(ReferenceElement.RowAbsolute Or ReferenceElement.ColumnAbsolute, Nothing)
+					Dim formula As String = String.Format("=RANK.AVG({0},R{1}C{2}:R{3}C{4})", cellRef, e.Cell.RowIndex + 1, 2, e.Cell.RowIndex + 1, 10)
+					Dim rank As Integer = CInt(Math.Truncate(spreadsheetControl1.Document.Evaluate(formula).NumericValue))
+					' The DevExpress.Docs.Text.NumberInWords class requires a reference to the DevExpress.Docs assembly. 
+					' To redistribute the DevExpress.Docs assembly the DevExpress Universal subscription or the Document Server license is required.
+					Dim rankText As String = DevExpress.Docs.Text.NumberInWords.Cardinal.ConvertToText(rank, DevExpress.Docs.Text.NumberCulture.Roman)
+					If rank > 0 AndAlso rank < 4 Then
+						e.Graphics.DrawString(rankText, headingFont, e.Cache.GetSolidBrush(Color.Red), e.Bounds.Left, e.Bounds.Top)
+					End If
+				End Using
 			End If
 		End Sub
 		#End Region ' #CustomDrawCell
 
 		#Region "#CustomDrawCellBackground"
-		Private Sub spreadsheetControl1_CustomDrawCellBackground(ByVal sender As Object, ByVal e As DevExpress.XtraSpreadsheet.CustomDrawCellBackgroundEventArgs)
+		Private Sub spreadsheetControl1_CustomDrawCellBackground(ByVal sender As Object, ByVal e As CustomDrawCellBackgroundEventArgs)
 			If e.Cell.HasFormula Then
 				e.Handled = True
 				Dim hBrush As New System.Drawing.Drawing2D.HatchBrush(System.Drawing.Drawing2D.HatchStyle.BackwardDiagonal, Color.LightGray, Color.White)
-				e.Graphics.FillRectangle(hBrush,e.FillBounds)
+				e.Graphics.FillRectangle(hBrush, e.FillBounds)
 			End If
 		End Sub
 		#End Region ' #CustomDrawCellBackground
